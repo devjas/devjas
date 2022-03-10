0758ce2ee08e9c8065564232ac2d23a2a090147d
devjas <30835414+devjas@users.noreply.github.com>
2022-03-10 17:03:44 -0500

    Add files via upload

---
+++ RedirectIfUserBlocked.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use DB;
+
+class RedirectIfUserBlocked
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+
+        // Redirects user to the blocked account message
+        $is_user_active = DB::table('users')->select('is_active')->where('id', auth()->id())->pluck('is_active');
+
+        foreach($is_user_active as $is_active) {
+            if($is_active !== 1) {
+                return redirect('account-blocked');
+            }
+        }
+        
+        return $next($request);
+    }
+}
