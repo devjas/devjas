ad33b61b2abefcc43dc0102c3d869ff0c03df32e
devjas <30835414+devjas@users.noreply.github.com>
2022-03-10 16:50:32 -0500

    Add files via upload

---
+++ DashboardPagesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Session;
+use DB;
+
+class DashboardPagesController extends Controller
+{
+    public function __construct() {
+        $this->middleware(['auth','verified']); // User must be loggedin and the E-Mail must be verified
+    }
+
+    public function getDashboard() {
+        return View::make('concert-back.pages.tours.show-tours');
+    }
+
+    public function remove_singer_from_all_tours($singer_id) {
+        
+        $singer = DB::table('singers')
+        ->where('id', $singer_id)
+        ->first();
+
+        if($singer->blkn_band_name) {
+            $dash = ' - ';
+        } else {
+            $dash = '';
+        }
+
+        DB::table('user_tour_singers')
+        ->where('blkn_singer_id', $singer_id)
+        ->delete();
+
+        Session::flash('success', "<b>".$singer->blkn_singer_firstname . " " . $singer->blkn_singer_lastname . $dash . $singer->blkn_band_name.'</b>' . ' has been removed from all Tours. Now you can delete this singer.');
+        return redirect()->back();
+
+    }
+}
