2826d4ecb968bbdc1a29a81cfedd032e706716b4
devjas <30835414+devjas@users.noreply.github.com>
2022-03-12 20:49:43 -0500

    Add files via upload

---
+++ middleware/TourInfo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+Illuminate\Database\Eloquent\SoftDeletes
+
+class TourInfo extends Model
+{
+    use HasFactory; 
+
+    use SoftDeletes;
+}
